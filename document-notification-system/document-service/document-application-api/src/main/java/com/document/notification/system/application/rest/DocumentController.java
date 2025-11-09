6b08931bf385a431804640021a5e5e002f02e18e
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2025-11-09 16:50:08 -0500

    base class for controller

---
+++ document-notification-system/document-service/document-application-api/src/main/java/com/document/notification/system/application/rest/DocumentController.java
@@ -0,0 +1,9 @@
+package com.document.notification.system.application.rest;
+
+/**
+ * @author Ivan Camilo Rincon Saavedra
+ * @version 1.0
+ * @since 9/11/2025
+ */
+public class DocumentController {
+}
