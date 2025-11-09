5a7ce1deffa41fe34220b2da5761075fd3d85c02
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2025-11-09 16:52:21 -0500

    Rest Annotations

--- document-notification-system/document-service/document-application-api/src/main/java/com/document/notification/system/application/rest/DocumentController.java
+++ document-notification-system/document-service/document-application-api/src/main/java/com/document/notification/system/application/rest/DocumentController.java
@@ -1,9 +1,16 @@
 package com.document.notification.system.application.rest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Ivan Camilo Rincon Saavedra
  * @version 1.0
  * @since 9/11/2025
  */
+@Slf4j
+@RestController
+@RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
 public class DocumentController {
 }
