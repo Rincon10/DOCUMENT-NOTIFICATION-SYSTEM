ae981fef48b7bdedcb00783a9b4f5a7df7a66c32
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2025-11-21 20:01:09 -0500

    Adding customer persistent layer

---
+++ document-notification-system/customer-service/customer-domain/customer-domain-core/src/main/java/com/document/notification/system/customer/service/entity/Customer.java
@@ -0,0 +1,12 @@
+package com.document.notification.system.customer.service.entity;
+
+import com.document.notification.system.domain.entity.AggregateRoot;
+import com.document.notification.system.domain.valueobject.CustomerId;
+
+/**
+ * @author Ivan Camilo Rincon Saavedra
+ * @version 1.0
+ * @since 21/11/2025
+ */
+public class Customer  extends AggregateRoot<CustomerId> {
+}
--- document-notification-system/document-service/document-domain/document-domain-core/src/main/java/com/document/notification/system/entity/Customer.java
+++ document-notification-system/document-service/document-domain/document-domain-core/src/main/java/com/document/notification/system/entity/Customer.java
@@ -2,16 +2,27 @@ package com.document.notification.system.entity;
 
 import com.document.notification.system.domain.entity.AggregateRoot;
 import com.document.notification.system.domain.valueobject.CustomerId;
+import lombok.*;
 
 /**
  * @author Ivan Camilo Rincon Saavedra
  * @version 1.0
  * @since 19/11/2025
  */
+@AllArgsConstructor
+@Getter
+@Setter
 public class Customer extends AggregateRoot<CustomerId> {
-    public Customer(CustomerId customerId) {
-        this.setId(customerId);
+    private String username;
+    private String firstName;
+    private String lastName;
+
+    @Builder
+    public Customer(CustomerId customerId, String username, String firstName, String lastName) {
+        super.setId(customerId);
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
-    public Customer() {}
 }
