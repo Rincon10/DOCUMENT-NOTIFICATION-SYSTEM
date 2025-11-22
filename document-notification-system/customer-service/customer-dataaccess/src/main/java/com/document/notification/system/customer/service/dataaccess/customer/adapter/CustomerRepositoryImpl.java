c66aa82d5fbf5c918581a847483ab0b05659928f
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2025-11-21 20:03:07 -0500

    Creating customer adapter  persisten layer to implement interface from core domain

---
+++ document-notification-system/customer-service/customer-dataaccess/src/main/java/com/document/notification/system/customer/service/dataaccess/customer/adapter/CustomerRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.document.notification.system.customer.service.dataaccess.customer.adapter;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Ivan Camilo Rincon Saavedra
+ * @version 1.0
+ * @since 21/11/2025
+ */
+@Component
+public class CustomerRepositoryImpl {
+}
