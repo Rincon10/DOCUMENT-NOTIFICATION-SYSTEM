c4d774ab8f3dc20c8d3917769a5bfebdb1d4578e
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2025-11-21 20:40:26 -0500

    adding adapter for dataacess layer for customer

--- document-notification-system/customer-service/customer-dataaccess/src/main/java/com/document/notification/system/customer/service/dataaccess/customer/adapter/CustomerRepositoryImpl.java
+++ document-notification-system/customer-service/customer-dataaccess/src/main/java/com/document/notification/system/customer/service/dataaccess/customer/adapter/CustomerRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.document.notification.system.customer.service.dataaccess.customer.adapter;
 
+import com.document.notification.system.customer.service.dataaccess.customer.mapper.CustomerDataAccessMapper;
+import com.document.notification.system.customer.service.dataaccess.customer.repository.CustomerJpaRepository;
 import com.document.notification.system.customer.service.entity.Customer;
 import com.document.notification.system.customer.service.ports.output.repository.CustomerRepository;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,17 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class CustomerRepositoryImpl implements CustomerRepository {
+    private final CustomerDataAccessMapper customerDataAccessMapper;
+    private final CustomerJpaRepository customerJpaRepository;
+
+    public CustomerRepositoryImpl(CustomerDataAccessMapper customerDataAccessMapper, CustomerJpaRepository customerJpaRepository) {
+        this.customerDataAccessMapper = customerDataAccessMapper;
+        this.customerJpaRepository = customerJpaRepository;
+    }
+
     @Override
     public Customer createCustomer(Customer customer) {
-        throw  new UnsupportedOperationException("Not implemented yet");
+        throw new UnsupportedOperationException("Not implemented yet");
 
     }
 }
