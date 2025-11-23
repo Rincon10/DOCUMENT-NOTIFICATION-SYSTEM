752bf26890c0997954a41adc02e38ec4c494c4d4
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2025-11-23 16:56:22 -0500

    d

--- document-notification-system/common/common-domain/src/main/java/com/document/notification/system/domain/valueobject/DocumentStatus.java
+++ document-notification-system/common/common-domain/src/main/java/com/document/notification/system/domain/valueobject/DocumentStatus.java
@@ -3,7 +3,7 @@ package com.document.notification.system.domain.valueobject;
 public enum DocumentStatus {
     PENDING,
     PROCESSING,
-    COMPLETED,
+    GENERATED,
     CANCELLING,
     CANCELLED
 }
