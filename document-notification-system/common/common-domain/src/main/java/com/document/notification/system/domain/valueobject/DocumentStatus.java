a038c6e5310a4a194bdae9265f0b67cf3e3e846d
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2025-11-23 18:44:46 -0500

    init-schema for document service

--- document-notification-system/common/common-domain/src/main/java/com/document/notification/system/domain/valueobject/DocumentStatus.java
+++ document-notification-system/common/common-domain/src/main/java/com/document/notification/system/domain/valueobject/DocumentStatus.java
@@ -2,8 +2,8 @@ package com.document.notification.system.domain.valueobject;
 
 public enum DocumentStatus {
     PENDING,
-    PROCESSING,
     GENERATED,
+    SENT,
     CANCELLING,
     CANCELLED
 }
--- document-notification-system/document-service/document-domain/document-domain-core/src/main/java/com/document/notification/system/document/service/domain/entity/Document.java
+++ document-notification-system/document-service/document-domain/document-domain-core/src/main/java/com/document/notification/system/document/service/domain/entity/Document.java
@@ -1,5 +1,6 @@
 package com.document.notification.system.document.service.domain.entity;
 
+import com.document.notification.system.document.service.domain.exception.DocumentDomainException;
 import com.document.notification.system.document.service.domain.valueobject.StreetAddress;
 import com.document.notification.system.domain.entity.AggregateRoot;
 import com.document.notification.system.domain.valueobject.CustomerId;
@@ -10,6 +11,8 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 /**
  * @author Ivan Camilo Rincon Saavedra
@@ -21,10 +24,11 @@ public class Document extends AggregateRoot<DocumentId> {
 
     public static final String FAILURE_MESSAGE_DELIMITER = ",";
     private final CustomerId customerId;
+
     private final StreetAddress deliveryAddress;
     private final DocumentType documentType;
     private final List<DocumentItem> documentItems;
-    private final DocumentStatus documentStatus;
+    private  DocumentStatus documentStatus;
     private List<String> failureMessages;
 
     @Builder
@@ -37,9 +41,34 @@ public class Document extends AggregateRoot<DocumentId> {
         this.documentStatus = documentStatus;
     }
 
-    public void validateDocument() {
+    public void initializeDocument() {
+        setId(new DocumentId(UUID.randomUUID()));
+        documentStatus = DocumentStatus.PENDING;
     }
 
-    public void initializeDocument() {
+    public void validateDocument() {
+        validateInitialDocument();
     }
+
+    public void generate(){
+        if (!DocumentStatus.PENDING.equals(this.documentStatus)) {
+            throw new DocumentDomainException("Document is not in correct state for generation!");
+        }
+        this.documentStatus = DocumentStatus.GENERATED;
+    }
+    public void sent(){
+        if (!DocumentStatus.GENERATED.equals(this.documentStatus)) {
+            throw new DocumentDomainException("Document is not in correct state for sent!");
+        }
+        this.documentStatus = DocumentStatus.SENT;
+    }
+
+    private void validateInitialDocument() {
+        if (Objects.nonNull(documentStatus) || Objects.nonNull(getId())) {
+            throw new DocumentDomainException("Document is not in correct state for initialization!");
+        }
+    }
+
+
+
 }
