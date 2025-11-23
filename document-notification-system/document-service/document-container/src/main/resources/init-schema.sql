329c5bc248dedf824408fd17a8fb1f79db2911b4
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2025-11-23 18:48:29 -0500

    Adding document sql

--- document-notification-system/document-service/document-container/src/main/resources/init-schema.sql
+++ document-notification-system/document-service/document-container/src/main/resources/init-schema.sql
@@ -8,3 +8,26 @@ DROP TYPE IF EXISTS order_status;
 CREATE TYPE order_status AS ENUM ('PENDING', 'GENERATED', 'SENT', 'CANCELLED', 'CANCELLING');
 
 DROP TABLE IF EXISTS "document".documents CASCADE;
+
+CREATE TABLE documents (
+    id UUID,
+    customer_id UUID,
+    account_id UUID,
+    file_name VARCHAR(255),
+    file_path VARCHAR(500),
+
+    period_start_date DATE NOT NULL,
+    period_end_date DATE NOT NULL,
+
+    total_late_interest NUMERIC(19,2),
+    total_regular_interest NUMERIC(19,2),
+    total_amount NUMERIC(19,2),
+
+    created_by VARCHAR(255),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+
+    document_status order_status NOT NULL,
+    failure_messages character varying COLLATE pg_catalog."default",
+    CONSTRAINT documents_pkey PRIMARY KEY (id)
+);
