41fb1af070724fd3e74f2024d55b129c650c90bc
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2025-11-23 16:10:52 -0500

    Adding handler controller improvement

--- document-notification-system/document-service/document-application-api/src/main/java/com/document/notification/system/application/exception/handler/DocumentExceptionHandler.java
+++ document-notification-system/document-service/document-application-api/src/main/java/com/document/notification/system/application/exception/handler/DocumentExceptionHandler.java
@@ -1,8 +1,14 @@
 package com.document.notification.system.application.exception.handler;
 
+import com.document.notification.system.document.service.domain.exception.DocumentDomainException;
+import com.document.notification.system.dto.ErrorDTO;
 import com.document.notification.system.exception.handler.GlobalExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * @author Ivan Camilo Rincon Saavedra
@@ -11,5 +17,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  */
 @Slf4j
 @ControllerAdvice
-public class DocumentExceptionHandler  extends GlobalExceptionHandler {
+public class DocumentExceptionHandler extends GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {DocumentDomainException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleException(DocumentDomainException documentDomainException) {
+        log.error(documentDomainException.getMessage(), documentDomainException);
+        return ErrorDTO.builder()
+                .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(documentDomainException.getMessage())
+                .build();
+    }
 }
