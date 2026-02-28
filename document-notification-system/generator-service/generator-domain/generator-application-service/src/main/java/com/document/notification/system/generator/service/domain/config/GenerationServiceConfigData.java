a0904afd387967e94f2780ae7db686ad5ebb823e
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2026-02-27 23:11:33 -0500

    generation part

---
+++ document-notification-system/generator-service/generator-domain/generator-application-service/src/main/java/com/document/notification/system/generator/service/domain/config/GenerationServiceConfigData.java
@@ -0,0 +1,13 @@
+package com.document.notification.system.generator.service.domain.config;
+
+import lombok.Data;
+
+/**
+ * @author Ivan Camilo Rincon Saavedra
+ * @version 1.0
+ * @since 27/02/2026
+ */
+@Data
+@Configuration
+public class GenerationServiceConfigData {
+}
