1597d46dd38a1f31b352a75dea4e4bb2831b8462
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2026-02-27 21:35:39 -0500

    generation domain exception

---
+++ document-notification-system/generator-service/generator-domain/generator-domain-core/src/main/java/com/document/notification/system/generator/service/domain/exception/GeneratorDomainException.java
@@ -0,0 +1,7 @@
+package com.document.notification.system.generator.service.domain.exception;
+
+public class GeneratorDomainException extends RuntimeException {
+    public GeneratorDomainException(String message) {
+        super(message);
+    }
+}
